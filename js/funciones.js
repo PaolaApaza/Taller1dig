9d0481b267bf98ea0838785b9cb2aa7e6d8a195b
PaolaApaza <paolaandreaapazaperalta.acad@gmail.com>
2024-04-01 18:06:21 -0400

    actualizacion

--- js/funciones.js
+++ js/funciones.js
@@ -1,5 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(){
-//EMPIEZA ACÀ
-
-
-});
+    //EMPIEZA ACÀ
+    let glide = new Glide(".glide",{
+        type:"carousel",
+        perView: 3,
+        gap:10,
+        autoplay:3000,
+        hoverpause:true
+    });
+    glide.mount()
+    
+    
+       
+    
+    });
